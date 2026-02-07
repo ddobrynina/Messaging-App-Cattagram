0b048ceddcbbb8f90b125c70d85898c14f4434dd
Daria Dobrynina <ddobrynina924@gmail.com>
2026-02-06 19:44:13 -0500

    Added a header to the messages page and a link back to the dashboard

---
+++ client/cattagram/src/components/messageHeader.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from 'react-router-dom';
+import {FaArrowLeft, FaPhoneAlt, FaEllipsisV} from 'react-icons/fa';
+import cat from '../assets/cat_avatar.jpg';
+
+
+const MessageHeader = () => {
+  return (
+    <div className="flex gap-4 justify-center items-center bg-[#57A8FF] text-white w-screen">
+        <NavLink to='/' >
+            <FaArrowLeft />
+        </NavLink>
+         <div><img className='rounded-[50%] h-15 w-15' src={cat} alt="" /></div>
+        <p>Friend</p>
+        <FaPhoneAlt />
+        <FaEllipsisV />
+    </div>
+  )
+}
+
+export default MessageHeader
--- client/cattagram/src/pages/Messages.jsx
+++ client/cattagram/src/pages/Messages.jsx
@@ -1,18 +1,25 @@
-import Message from '../components/message'
-import messages from './messages.json'
+import MessageHeader from '../components/messageHeader';
+import Message from '../components/message';
+import UserMessage from '../components/userMessage';
+import messages from './messages.json';
 
 const Messages = () => {
   
   const messagesArray = messages;
-  
+
   return (
-    <div className='bg-linear-120 from-[#3EB37C] to-[#D6FFE7] w-screen h-screen'>
+    <>
+      <MessageHeader />
+      <div className='bg-linear-120 from-[#3EB37C] to-[#D6FFE7] w-screen h-screen'>
       {
         messagesArray.messages.map((message) => 
-          <Message key={message.id} message={message} />
+          message.currentUser? 
+            <UserMessage key={message.id} message={message} /> :
+            <Message key={message.id} message={message} />
         )
       }
     </div>
+    </>
   )
 }
 
